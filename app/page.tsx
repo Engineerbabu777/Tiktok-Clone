9de98a66dbccb22e9f65d4a6e12450a91e016cc8
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-21 21:20:33 +0500

    added-home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
+"use client"
 
+import { useEffect } from "react"
+import MainLayout from "./layouts/MainLayout"
+import { usePostStore } from "@/app/stores/post"
+import ClientOnly from "./components/ClientOnly"
+import PostMain from "./components/PostMain"
 
-
-import MainLayout from './layouts/MainLayout';
-
-export default function Home(){
-
-
-  return(<>
-
-
-<MainLayout>
- <div className="">
-Home
- </div>
-</MainLayout>
-  </>)
+export default function Home() {
+  let { allPosts, setAllPosts } = usePostStore();
+  useEffect(() => { setAllPosts()}, [])
+  return (
+    <>
+      <MainLayout>
+        <div className="mt-[80px]  w-[calc(100%-90px)] max-w-[690px] ml-auto">
+          <ClientOnly>
+            {allPosts.map((post, index) => (
+              <PostMain post={post} key={index} />
+            ))}
+          </ClientOnly>
+        </div>
+      </MainLayout>
+    </>
+  )
 }
